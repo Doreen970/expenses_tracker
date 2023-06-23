f3aa5933ae2fcd34167151ca72eb06355f3e02a4
Doreen970 <doreenwanyama20@gmail.com>
2023-06-23 13:27:25 +0300

    included the sheet name for the data frame to read

--- expenses.py
+++ expenses.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 excel_file = "expenditure.xlsx"
-df = pd.read_excel(excel_file)
+df = pd.read_excel(excel_file, sheet_name='EXPENSES TRACKER')
 #convert to csv file
 csv_file = "new_file.csv"
 df.to_csv(csv_file, index=False)
+
+print(df)
